2a99a4e3ca2065bf540abaf99b764155394b877e
Chaitanya Mandale <chaitanyam187@gmail.com>
2026-05-24 19:45:23 +0530

    test

--- api/Controllers/UserController.cs
+++ api/Controllers/UserController.cs
@@ -22,7 +22,7 @@ namespace RevloDB.Controllers
         public async Task<ActionResult<UserDto>> GetUser(int userId)
         {
             var currentUserId = ControllerUtil.GetUserIdFromHTTPContext(HttpContext);
-            Console.WriteLine("test1");
+            Console.WriteLine("test2");
             if (currentUserId != userId)
             {
                 return this.ForbiddenProblem("You can only access your own user information");
